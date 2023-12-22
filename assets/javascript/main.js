fd1d7ea256aea71d661908a983b20de96352bd7d
maylique <supercoder@AirvonSperCoder.fritz.box>
2023-12-22 14:29:48 +0100

    rgb randomizer added

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+
+
+const colorRandomizer = () => {
+    let red = Math.floor((Math.random()) * 256 )+ 1
+    let green = Math.floor((Math.random()) * 256 )+ 1
+    let blue = Math.floor((Math.random()) * 256 )+ 1
+
+    let rgb = [red, green, blue]
+    return rgb
+}
+
+
+
